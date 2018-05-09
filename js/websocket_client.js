ac6652cb3b606af191ebeb101e40fb4951f80e63
Nicolas Gonzalez <ngonzalez@snootlab.com>
2018-05-09 09:25:26 +0200

    Add connection form

---
+++ js/websocket_client.js
@@ -0,0 +1,78 @@
+
+var websocketclient = {
+    'client': null,
+    'lastMessageId': 1,
+    'lastSubId': 1,
+    'subscriptions': [],
+    'messages': [],
+    'connected': false,
+
+    'connect': function () {
+
+        var host = $('#urlInput').val();
+        var path = $('#pathInput').val();
+        var port = parseInt($('#portInput').val(), 10);
+        var clientId = "web_" + parseInt(Math.random() * 100, 10)
+        var username = $('#userInput').val();
+        var password = $('#pwInput').val();
+        var keepAlive = parseInt($('#keepAliveInput').val());
+        var cleanSession = $('#cleanSessionInput').is(':checked');
+        var ssl = $('#sslInput').is(':checked');
+
+        this.client = new Paho.MQTT.Client(host, port, path, clientId);
+        this.client.onConnectionLost = this.onConnectionLost;
+        this.client.onMessageArrived = this.onMessageArrived;
+
+        var options = {
+            timeout: 3,
+            keepAliveInterval: keepAlive,
+            cleanSession: cleanSession,
+            useSSL: ssl,
+            onSuccess: this.onConnect,
+            onFailure: this.onFail
+        };
+
+        if (username.length > 0) {
+            options.userName = username;
+        }
+        if (password.length > 0) {
+            options.password = password;
+        }
+
+        this.client.connect(options);
+    },
+
+    'onConnect': function () {
+        websocketclient.connected = true;
+        console.log("connected");
+
+        $( "#mqtt_state_indicator" ).html( "Connected" );
+        $( "#mqtt_state_indicator" ).removeClass( "badge-info" );
+        $( "#mqtt_state_indicator" ).removeClass( "badge-danger" );
+        $( "#mqtt_state_indicator" ).addClass( "badge-success" );
+    },
+
+    'onFail': function (message) {
+        websocketclient.connected = false;
+        console.log("error: " + message.errorMessage);
+    },
+
+    'onConnectionLost': function (responseObject) {
+        websocketclient.connected = false;
+        if (responseObject.errorCode !== 0)
+        {
+          $( "#mqtt_state_indicator" ).html( "Lost connection ..." );
+          $( "#mqtt_state_indicator" ).removeClass( "badge-info" );
+          $( "#mqtt_state_indicator" ).removeClass( "badge-success" );
+          $( "#mqtt_state_indicator" ).addClass( "badge-danger" );
+        }
+    },
+
+    'onMessageArrived': function (message) {
+        console.log("onMessageArrived:" + message.payloadString + " qos: " + message.qos);
+    },
+
+    'disconnect': function () {
+        this.client.disconnect();
+    },
+};
